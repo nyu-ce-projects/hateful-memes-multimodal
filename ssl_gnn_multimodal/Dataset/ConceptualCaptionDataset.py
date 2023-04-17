9b389c6e6a73220419d1cffe220047709463e6a6
Akash Mishra <akash.mishra13@gmail.com>
2023-04-17 04:14:48 -0400

    feat: Conceptual Caption Dataset Class Added

---
+++ ssl_gnn_multimodal/Dataset/ConceptualCaptionDataset.py
@@ -0,0 +1,45 @@
+import os
+import json
+import torch
+from PIL import Image
+from glob import glob
+from pathlib import Path
+
+class ConceptualCaptionDataset(torch.utils.data.Dataset):
+    def __init__(self,data_path,image_transform,tokenizer) -> None:
+        super().__init__()
+        self.data = glob("../datasets/cc12m/*/*.jpg", recursive = True)
+        self.data_dir = data_path
+        self.image_transform = image_transform
+        self.tokenizer = tokenizer     
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        # Load images on the fly.
+        image = Image.open(os.path.join(self.data[index])).convert("RGB")
+        text_file_path = os.path.basename(self.data[index]).replace("jpg","txt")
+        text = Path(text_file_path).read_text().replace('\n','')
+        
+        return image, text
+    
+    def collate_fn(self,batch):
+        # Image Tensor
+        tensor_img = torch.stack(
+            [self.image_transform(row[0]) for row in batch]
+        )
+
+        # Tokenized Text Tensor 
+        encoded_queries = self.tokenizer([row[1] for row in batch])
+        lens = [len(row) for row in encoded_queries['input_ids']]
+        text_tensor = torch.zeros(len(batch),max(lens),dtype=torch.long)
+        attention_mask = torch.zeros(len(batch),max(lens),dtype=torch.long)
+        
+        for i_batch in range(len(batch)):
+            length = lens[i_batch]
+            text_tensor[i_batch, :length] = torch.tensor(encoded_queries['input_ids'][i_batch])
+            attention_mask[i_batch, :length] = torch.tensor(encoded_queries['attention_mask'][i_batch])
+        
+
+        return tensor_img,text_tensor,attention_mask
