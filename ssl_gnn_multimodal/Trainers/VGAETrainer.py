54b09b5e60525090ea6f11d92bb0460ef8346fae
Akash Mishra <akash.mishra13@gmail.com>
2023-04-27 02:08:15 -0400

    fix: metrics corrected

--- ssl_gnn_multimodal/Trainers/VGAETrainer.py
+++ ssl_gnn_multimodal/Trainers/VGAETrainer.py
@@ -6,7 +6,7 @@ from Models.GraphClassifier import GraphClassifier
 import torch
 from torch_geometric.utils import negative_sampling
 from torch_geometric.nn import MLP, MLPAggregation,SetTransformerAggregation,DeepSetsAggregation,GRUAggregation
-from sklearn.metrics import average_precision_score, roc_auc_score
+from sklearn.metrics import average_precision_score, roc_auc_score,accuracy_score,f1_score
 import numpy as np
 from config import PROJECTION_DIM
 
@@ -70,10 +70,9 @@ class VGAETrainer(MMGNNTrainer):
         self.setEval()
         test_loss = 0
         total = 0
-        roc_auc_scores = []
-        ap_scores = []
-        micro_f1s = []
-        accuracies = []
+        preds = None
+        proba = None
+        out_label_ids = None
         with torch.no_grad():
             for images, tokenized_text, attention_masks, labels in data_loader:
                 images, tokenized_text, attention_masks, labels = images.to(self.device), tokenized_text.to(self.device), attention_masks.to(self.device), labels.to(self.device)
@@ -92,35 +91,66 @@ class VGAETrainer(MMGNNTrainer):
                 if self.pretrain is True:
                 # Pretraining  
                     loss = self.models['graph'].loss(z, g_data)
-                    roc_auc,ap_score = self.models['graph'].metrics(z, g_data)
+                    
+                    neg_edge_index = negative_sampling(g_data.edge_index, z.size(0))
+        
+                    pos_y = z.new_ones(g_data.edge_index.size(1))
+                    neg_y = z.new_zeros(neg_edge_index.size(1))
+                    y = torch.cat([pos_y, neg_y], dim=0)
+
+                    pos_pred = self.decoder(z, g_data.edge_index, sigmoid=True)
+                    neg_pred = self.decoder(z, neg_edge_index, sigmoid=True)
+                    pred = torch.cat([pos_pred, neg_pred], dim=0)
+                    if proba is None:
+                        proba = pred.detach().cpu().numpy() 
+                    else:
+                        proba = np.append(proba,pred.detach().cpu().numpy(),axis=0)
+                    
+                    if preds is None:
+                        preds = pred.detach().cpu().numpy() > 0.5
+                    else:
+                        preds = np.append(preds,pred.detach().cpu().numpy()> 0.5,axis=0)
+                        
+                    if out_label_ids is None:
+                        out_label_ids = y.detach().cpu().numpy()
+                    else:
+                        out_label_ids = np.append(out_label_ids,y.detach().cpu().numpy())
                     
                 else:
                 # hateful classification
                     output = self.models['classifier'](z,g_data)
                     loss = self.criterion(output, g_data.y)
+
                     # Metrics Calculation
-                    roc_auc,ap_score,micro_f1,accuracy = self.models['classifier'].metrics(output,labels)
-                    micro_f1s.append(micro_f1)
-                    accuracies.append(accuracy)
+                    if preds is None:
+                        preds = torch.sigmoid(output).detach().cpu().numpy() > 0.5
+                    else:
+                        preds = np.append(preds, torch.sigmoid(output).detach().cpu().numpy() > 0.5, axis=0)
+                    if proba is None:
+                        proba = torch.sigmoid(output).detach().cpu().numpy()
+                    else:
+                        proba = np.append(proba, torch.sigmoid(output).detach().cpu().numpy(), axis=0)
+                    
+                    if out_label_ids is None:
+                        out_label_ids = labels.detach().cpu().numpy()
+                    else:
+                        out_label_ids = np.append(out_label_ids, labels.detach().cpu().numpy(), axis=0)
 
                 test_loss += loss.item()
                 total += labels.size(0)
 
-                roc_auc_scores.append(roc_auc)
-                ap_scores.append(ap_score)
-
         metrics = {
             "loss": test_loss/total,
-            "auc": round(np.mean(roc_auc_scores),3),
-            "avg_precision": round(np.mean(ap_scores),3)
+            "auc": round(roc_auc_score(out_label_ids,proba),3),
+            "avg_precision": round(average_precision_score(out_label_ids,proba),3),
+            "accuracy":round(accuracy_score(out_label_ids, preds),3),
+            "micro_f1":round(f1_score(out_label_ids, preds, average="micro"),3)
         }
-        if len(accuracies)>0:
-            metrics['accuracy'] = round(np.mean(accuracies),3)
-        
-        if len(micro_f1s)>0:
-            metrics['micro_f1'] = round(np.mean(micro_f1s),3)
+        # if self.pretrain is not True:
+        #     metrics['accuracy'] = round(accuracy_score(out_label_ids, preds),3)
+        #     metrics['micro_f1'] = round(f1_score(out_label_ids, preds, average="micro"),3)
 
-        print("{} --- Epoch : {} | roc_auc_score : {} | average_precision_score : {}".format(data_type,epoch,metrics['auc'],metrics['avg_precision']))    
+        print("{} --- Epoch : {} | roc_auc_score : {} | average_precision_score : {} | accuracy : {} | micro_f1 : {}".format(data_type,epoch,metrics['auc'],metrics['avg_precision'],metrics['accuracy'],metrics['micro_f1']))    
         return metrics                       
 
     def save_checkpoint(self,epoch, metrics):
